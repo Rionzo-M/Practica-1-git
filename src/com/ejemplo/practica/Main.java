853221892fda5285479d9057f75288340f2c076b
Rionzo-M <gameroborreroalfonso@gmail.com>
2026-01-12 12:46:55 +0100

    Practica1_git

---
+++ src/com/ejemplo/practica/Main.java
@@ -0,0 +1,9 @@
+package com.ejemplo.practica;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Alfonso Borrero Gamero");
+        System.out.println("Proyecto Git inicial funcionando correctamente");
+    }
+}
